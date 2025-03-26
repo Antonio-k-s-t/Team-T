14d6d82a3685e64db2f5260dc635e581ea62d805
Antonio-k-s-t <antoniohristov585@gmail.com>
2025-03-26 19:20:06 +0200

    логин-дизайн

--- SAP-task/src/Create.java
+++ SAP-task/src/Create.java
@@ -12,6 +12,19 @@ public class Create extends JFrame{
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
     setLayout(null);
+    //logo
+        ImageIcon imageIcon = new ImageIcon("Trohar Delivar-modified.png"); // път към снимката
+        Image scaledImage = imageIcon.getImage().getScaledInstance(300, 250, Image.SCALE_SMOOTH); // нов размер
+        ImageIcon resizedIcon = new ImageIcon(scaledImage);
+        JLabel imageLabel1 = new JLabel(resizedIcon);
+        imageLabel1.setBounds(100, 10, 200, 300); // същите размери като на scaledInstance
+        add(imageLabel1);
+
+    //hedar
+        JPanel coloredPanel1 = new JPanel();
+        coloredPanel1.setBounds(0, 0, 2800, 264); // позиция и размер
+        coloredPanel1.setBackground(new Color(246, 214, 165)); // цвят на фона
+        add(coloredPanel1);
     JLabel novPotrebitel = new JLabel(" Нов Потребител:");
         novPotrebitel.setFont(new Font("SansSerif", Font.PLAIN, 14));
     add(novPotrebitel);
@@ -25,17 +38,16 @@ public class Create extends JFrame{
     add(novVernaparola);
     JPasswordField novGreshnaparola = new JPasswordField();
     add(novGreshnaparola);
-        JButton CreatButton=new JButton("Създай");
-        add(CreatButton);
-//Бутон вилица
+
+//Бутон parvola
         // Зареждане и мащабиране на нормалната снимка
-        ImageIcon originalIcon = new ImageIcon("vilica1.png");
-        Image scaledNormal = originalIcon.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+        ImageIcon originalIcon = new ImageIcon("parjola1.png");
+        Image scaledNormal = originalIcon.getImage().getScaledInstance(130, 130, Image.SCALE_SMOOTH);
         ImageIcon normalIcon = new ImageIcon(scaledNormal);
 
 // Зареждане и мащабиране на hover снимката
-        ImageIcon originalHover = new ImageIcon("vilica2.png");
-        Image scaledHover = originalHover.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+        ImageIcon originalHover = new ImageIcon("parjla2.png");
+        Image scaledHover = originalHover.getImage().getScaledInstance(130, 130, Image.SCALE_SMOOTH);
         ImageIcon hoverIcon = new ImageIcon(scaledHover);
 
 // Създаване на бутона
@@ -53,12 +65,12 @@ public class Create extends JFrame{
             @Override
             public void componentResized(ComponentEvent e) {
                 int center2 = getWidth() / 2;
-                novPotrebitel.setBounds(center2 - 150, 100, 100, 25);
-                novZaguben.setBounds(center2 - 50, 100, 160, 25);
-                novVernaparola.setBounds(center2 - 150, 140, 100, 25);
-                novGreshnaparola.setBounds(center2 - 50, 140, 160, 25);
-                CreatButton.setBounds(center2 -50, 190, 140, 25);
-                imageButton.setBounds(center2-50, 210, 150, 150); // позиция и размер
+                novPotrebitel.setBounds(center2 - 150, 180+100, 100, 25);
+                novZaguben.setBounds(center2 - 50, 180+100, 160, 25);
+                novVernaparola.setBounds(center2 - 150, 180+140, 100, 25);
+                novGreshnaparola.setBounds(center2 - 50, 180+140, 160, 25);
+                imageLabel1.setBounds(center2-150, -90, 300, 450);
+                imageButton.setBounds(center2-50, 180+180, 120, 100); // позиция и размер
             }
         });
         setVisible(true);
