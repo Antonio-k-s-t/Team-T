0f1c93d7db3abd143018d9ebc5d4714f5573d486
v4sse <vaninov@tu-sofia.bg>
2025-04-12 18:08:12 +0300

    Update Food.java

--- SAP-task/src/trohardelivery/entity/Food.java
+++ SAP-task/src/trohardelivery/entity/Food.java
@@ -3,27 +3,24 @@ package trohardelivery.entity;
 import java.util.HashMap;
 
 abstract class Food{
-    //TODO: КАКВО трябва да значи продукт в този смисъл
-    protected String product;
+    protected String productName;
     protected String code;
     protected float price;
     protected int quantity;
 
-    private final static HashMap<String, String> information = new HashMap<>();
-
-    protected Food(String product, String code, float price, int quantity) {
-        this.product = product;
+    protected Food(String productName, String code, float price, int quantity) {
+        this.productName = productName;
         this.code = code;
         this.price = price;
         this.quantity = quantity;
     }
 
     public String getProduct() {
-        return product;
+        return productName;
     }
 
-    protected void setProduct(String product) {
-        this.product = product;
+    protected void setProduct(String productName) {
+        this.productName = productName;
     }
 
     public String getCode() {
@@ -48,30 +45,6 @@ abstract class Food{
 
     protected void setQuantity(int quantity) {
         this.quantity = quantity;
-    }
-
-    protected void setInfo(String productInformation)
-    {
-        information.put(this.product, productInformation);
-    }
-
-    public String getProductInfo(){
-        if(information.containsKey(product)){
-        return information.get(product);
-        }
-        else{
-            return "Няма информация за такъв продукт";
-        }
-    }
-
-    protected void removeProduct(String pName)
-    {
-        if(pName.equals(product) && information.containsKey(pName)){
-            information.remove(pName);
-        }
-        else{
-            System.err.println("Не съществува такъв продукт за премахване!");
-        }
     }
 }
 
