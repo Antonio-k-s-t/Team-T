ec53ebaaa5a1145fb6176e19e066acced5d9fbe4
DiGenov <dgenov0309@abv.bg>
2025-04-23 18:15:37 +0300

    add spring security configuration

---
+++ trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/config/SpringSecurityConfig.java
@@ -0,0 +1,20 @@
+package bg.sap.trohar_delivery.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+}
