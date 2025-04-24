0c266acda97793361dad171b5e8df20c24d4ffff
Komandata <kristiqnkanchev12@gmail.com>
2025-04-24 21:26:08 +0300

    Fixed link path

--- trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/controller/MenuController.java
+++ trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/controller/MenuController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MenuController {
-    @GetMapping("мену")
+    @GetMapping("/menu")
     public String showMenu() {
 
         return "menu";
