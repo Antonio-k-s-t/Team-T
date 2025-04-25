ad474a603fd155e94e7573b7c9021f412ef031d1
v4sse <vaninov@tu-sofia.bg>
2025-04-25 15:18:23 +0300

    Update CartService.java

--- trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/service/CartService.java
+++ trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/service/CartService.java
@@ -6,13 +6,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import bg.sap.trohar_delivery.repository.CartRepository;
 import bg.sap.trohar_delivery.repository.MenuRepository;
+import bg.sap.trohar_delivery.model.Order;
 
 @Service
 public class CartService {
     @Autowired
     private CartRepository cartRepository;
 
-    public List<Food> getCartItems(Long userId) {
+    public List<Menu> getCartItems(Long userId) {
         return cartRepository.findByUserId(userId);
     }
 
@@ -20,7 +21,18 @@ public class CartService {
         cartRepository.addItem(userId, productId);
     }
 
-    public void placeOrder(Long userId) {
-        cartRepository.placeOrder(userId);
-    }
+    public Order placeOrder(Long orderID, Driver driver, Customer customer,
+                           Date createdOrderDate, Date finishedOrderDate,
+                           String status, List<Menu> menu) {
+        double total = products.stream().mapToDouble(Menu::getPrice).sum();
+        Oder order = new Order();
+        order.setId(orderID);
+        order.setCustomer(customer);
+        order.setDriver(driver);
+        order.setTotal(total);
+        order.setCreatedDate(createdOrderDate);
+        order.setFinishedDate(finishedOrderDate);
+        order.setStatus(status);
+        order.setRestaurants(restaurants);
+
 }
