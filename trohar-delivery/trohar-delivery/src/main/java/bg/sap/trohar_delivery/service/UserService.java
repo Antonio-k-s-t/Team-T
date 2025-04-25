8ba5d62f964ca9ba64482943a540f8507d93b303
Vasyl <vasilninov04@gmail.com>
2025-04-25 18:31:14 +0300

    Changes in UserService

--- trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/service/UserService.java
+++ trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/service/UserService.java
@@ -10,16 +10,44 @@ import bg.sap.trohar_delivery.repository.UserRepository;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     public User registerUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        String hashedPassword = passwordEncoder.encode(req.getPassword());
+        user.setPassword(hashedPassword);
+
         return userRepository.save(user);
     }
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
+
+    public List<Customer> getAllCustomers(){
+        return userRepository.findAll().stream().filter(user -> user instanceof Customer)
+                .map((user -> (Customer) user).toList();
     }
+
+    public List<Driver> getAllDrivers(){
+        return userRepository.findAll().stream().filter(user -> user instanceof Driver)
+                .map((user -> (Driver) user).toList();
+    }
+
+    public Optional<User> getUserByUsername(String username)
+    {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username" + username + "not found!"));
+    }
+
+    public Optional<User> getByEmail(String email)
+    {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new EmailNotFoundException("Email" + email + "not found"));
+    }
+
     public boolean isBlacklisted(User user) {
         return user.isBlacklisted();
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
     }
 }
