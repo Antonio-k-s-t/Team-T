7d4516846f5d76f7ad0d8cf757d6fdcdaa7f11dc
v4sse <vaninov@tu-sofia.bg>
2025-04-25 10:58:04 +0300

    Update ProfileRepository.java

--- trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/repository/ProfileRepository.java
+++ trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/repository/ProfileRepository.java
@@ -4,10 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-import bg.sap.trohar_delivery.model.Customer;
+import bg.sap.trohar_delivery.model.User;
 
 @Repository
-public interface ProfileRepository extends JpaRepository<Customer, Long>{
-    Optional<Customer> findByUsername(String username);
+public interface ProfileRepository extends JpaRepository<User, Long>{
+    Optional<User> findByRoleAndUsername(String role, String username);
 }
 
