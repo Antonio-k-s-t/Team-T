69a74d1489a180ae58a436dee244dc122750981b
DiGenov <dgenov0309@abv.bg>
2025-04-25 18:58:12 +0300

    fixed UserService

--- trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/service/UserService.java
+++ trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/service/UserService.java
@@ -1,5 +1,8 @@
 package bg.sap.trohar_delivery.service;
 
+import bg.sap.trohar_delivery.model.Customer;
+import bg.sap.trohar_delivery.model.User;
+import bg.sap.trohar_delivery.model.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,6 +26,7 @@ public class UserService {
 
     public List<Customer> getAllCustomers(){
         return userRepository.findAll().stream().filter(user -> user instanceof Customer)
+                .map((user -> (Customer) user).toList();
                 .map(user -> (Customer) user)
                 .toList();
     }
@@ -33,23 +37,24 @@ public class UserService {
                 .map(user -> (Driver) user)
                 .toList();
 
-    public Optional<User> getUserByUsername(String username)
-    {
-        return userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username" + username + "not found!"));
-    }
+        public Optional<User> getUserByUsername (String username)
+        {
+            return userRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("Username" + username + "not found!"));
+        }
 
-    public Optional<User> getByEmail(String email)
-    {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new EmailNotFoundException("Email" + email + "not found"));
-    }
+        public Optional<User> getByEmail (String email)
+        {
+            return userRepository.findByEmail(email)
+                    .orElseThrow(() -> new EmailNotFoundException("Email" + email + "not found"));
+        }
 
-    public boolean isBlacklisted(User user) {
-        return user.isBlacklisted();
-    }
+        public boolean isBlacklisted (User user){
+            return user.isBlacklisted();
+        }
 
-    public User saveUser(User user){
-        return userRepository.save(user);
+        public User saveUser (User user){
+            return userRepository.save(user);
+        }
     }
 }
