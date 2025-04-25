51e4fda739fe3b0088f445fc4ba75b67e9bedad0
v4sse <vaninov@tu-sofia.bg>
2025-04-25 15:16:29 +0300

    Update CartRepository.java

--- trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/repository/CartRepository.java
+++ trohar-delivery/trohar-delivery/src/main/java/bg/sap/trohar_delivery/repository/CartRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import bg.sap.trohar_delivery.model.Order;
 
 public interface CartRepository {
-    List<Food> findByUserId(Long userId);
+    List<Menu> findByUserId(Long userId);
     void addItem(Long userId, Long productId);
     void placeOrder(Long orderID, Driver driver, Customer customer,
                     Date createdOrderDate, Date finishedOrderDate,
